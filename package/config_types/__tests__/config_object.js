d73230e210b650fe738909bf69d2e105942002bc
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-11-13 11:28:35 +0000

    Add tests for new config types and utils

---
+++ package/config_types/__tests__/config_object.js
@@ -0,0 +1,41 @@
+/* global test expect */
+
+const ConfigObject = require('../config_object')
+
+test('new', () => {
+  expect(new ConfigObject()).toBeInstanceOf(ConfigObject)
+})
+
+test('set', () => {
+  const object = new ConfigObject()
+  expect(object.set('key', 'value')).toEqual({ key: 'value' })
+})
+
+test('get', () => {
+  const object = new ConfigObject()
+  object.set('key', 'value')
+  object.set('key1', 'value1')
+  expect(object.get('key')).toEqual('value')
+})
+
+test('delete', () => {
+  const object = new ConfigObject()
+  object.set('key', { key1: 'value' })
+  expect(object.delete('key.key1')).toEqual({ key: {} })
+  expect(object.delete('key')).toEqual({})
+})
+
+test('toObject', () => {
+  const object = new ConfigObject()
+  object.set('key', 'value')
+  object.set('key1', 'value1')
+  expect(object.toObject()).toEqual({ key: 'value', key1: 'value1' })
+})
+
+test('merge', () => {
+  const object = new ConfigObject()
+  object.set('foo', {})
+  expect(object.merge({ key: 'foo', value: 'bar' })).toEqual(
+    { foo: {}, key: 'foo', value: 'bar' }
+  )
+})
